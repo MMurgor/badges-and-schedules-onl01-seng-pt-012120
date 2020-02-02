b39c8dcbc23619e30c47334a84b4f4358bdb53a2
Romena Murgor <romenamurgor@gmail.com>
2020-02-02 02:15:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,6 @@ badge_maker ("Arel")
 => "Hello, my name is Arel."
 batch_badge_creator
 assign_rooms "Hello___! You'll be assigned to room___!"
-
+each_with_index
                 
                 
