31ed73c23ded776e29fbf616ce908e5c915544f7
Romena Murgor <romenamurgor@gmail.com>
2020-02-02 02:06:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,17 @@
 #conference_badges.rb
 badge_maker ("Arel")
 => "Hello, name is Arel."
+require 'spec_helper'
+
+describe 'conference_badges' do
+
+  let(:name) {"Arel"}
+  let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+  let(:badges) {[
+                  "Hello, my name is Edsger.",
+                  "Hello, my name is Ada.",
+                  "Hello, my name is Charles.",
+                  "Hello, my name is Alan.",
+                  "Hello, my name is Grace.",
+                  "Hello, my name is Linus.",
+                  "Hello, my name is Matz."
